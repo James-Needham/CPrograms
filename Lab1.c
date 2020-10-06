47056500714d670fc713134dcae66253d6dfeaf9
James-Needham <james.needham@ucdconnect.ie>
2020-10-06 15:33:18 +0100

    Adding the c file associated with Lab 1

---
+++ Lab1.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("Hello\t\tWorld!!\nGoodbye\t\tWorld!!");
+  return 0;
+}
+
+
+int main(void)
+{
+  printf("o");
+  printf("n");
+  printf("e");
+  printf("\n");
+  printf("t");
+  printf("w");
+  printf("o");
+  printf("\t");
+  return 0;
+}
+
+
+int main(void)
+{
+  printf("========================\n");
+  printf("=                      =\n");
+  printf("= She sells sea shells =\n");
+  printf("=                      =\n");
+  printf("========================\n");
+  return 0;
+}
+
+int main(void)
+{
+  printf("one\n");
+  printf("two\n");
+  printf("three");
+
+  return 0;
+}
+
+int main(void)
+{
+  printf("one\ntwo");
+  return 0;
+}
